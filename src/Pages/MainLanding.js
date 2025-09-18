e92e845fad453441a21e0776b2a069f8db4d6fe6
subash-creator <subash@soaron.co>
2025-09-18 10:39:11 +0530

    update

--- src/Pages/MainLanding.js
+++ src/Pages/MainLanding.js
@@ -6,7 +6,7 @@ import "./MainLanding.css";
 import Header from "../Component/Header";
 import Footer from "../Component/Footer";
 export default function MainLanding() {
-    return(
+    return (
         <div className="main-landing-container">
             <Helmet>
                 <title>Soaron</title>
@@ -16,8 +16,14 @@ export default function MainLanding() {
                 />
             </Helmet>
             <Header />
-            <section className="main-landing-hero">
-                </section>
+            <section className="section-1">
+                <h1>Revolutionizing Construction with Drone Technology</h1>
+                <p>Autonomous drones for precision painting, industrial coating, and high-rise cleaning — safer, faster, and smarter.</p>
+                <div className="button-group">
+                    <button className="waitlist">Join Our Waitlist</button>
+                    <button className="Partner">Partner With Us</button>
+                </div>
+            </section>
             <Footer />
         </div>
     );
