0aea13362ae6755e3fbbea6c1a1bc2996f953683
Subashlearner <subash.2003.21.10@gmail.com>
2025-09-30 12:30:33 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Career from "./Pages/Career";
 import Press from "./Pages/Press";
 import Colorsownspray from "./Pages/Colorsownspray";
 import MainLanding from "./Pages/MainLanding";
+import ComingSoon from "./Pages/ComingSoon";
 function App() {
   return (
     <Router>
@@ -18,7 +19,9 @@ function App() {
         {/* Press & Media page */}
         <Route path="/press" element={<Press />} />
         {/* Colorown Spray page */}
-        <Route path="/Colorsownspray" element={<Colorsownspray />} />
+        <Route path="/Colorsownspray" element={<Colorsownspray />} /> 
+         <Route path="/ComingSoon" element={<ComingSoon />} /> 
+        
       </Routes>
     </Router>
   );
